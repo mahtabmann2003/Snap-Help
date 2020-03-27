e1359dc738a14d0a2ed6d96dbcd5f82c3b23d08d
mahtabmann2003 <47609548+mahtabmann2003@users.noreply.github.com>
2020-03-27 18:14:11 -0400

    yuh

--- hospital-health.js
+++ hospital-health.js
@@ -1,25 +1,58 @@
-
 var travel;
 var fever;
 var tiredness;
 var cough;
 var pressure;
 var heart;
+var breath;
+var resp;
 var diabetes;
 var country;
 var age;
+const countryCode = localStorage.getItem('Country');
+var cases;
+var recovered;
+var death;
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-19-data.p.rapidapi.com/country/code?format=undefined&code=" + countryCode,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "c6ec7d9f25msh0283764d9ae1e21p142776jsn1298bb50990e"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    country = response[0].country;
+    cases = response[0].confirmed;
+    recovered = response[0].recovered;
+    death = response[0].deaths;
+    printStuff();
+});
+
+function printStuff() {
+    document.getElementById("Country").innerHTML = country;
+    document.getElementById("Active").innerHTML = cases;
+    document.getElementById("Recovered").innerHTML = recovered;
+    document.getElementById("Deaths").innerHTML = death;
+}
 
 function checkup() {
   travel = document.getElementById("travel").checked;
+  breath = document.getElementById("breath").checked;
+  resp = document.getElementById("resp").checked;
   fever = document.getElementById("fever").checked;
   tiredness = document.getElementById("tiredness").checked;
   cough = document.getElementById("cough").checked;
   pressure = document.getElementById("pressure").checked;
   heart = document.getElementById("heart").checked;
   diabetes = document.getElementById("diabetes").checked;
-  country = localStorage.getItem('Country');
   age = localStorage.getItem('Age');
-  if(travel === true){
+  if(travel === true || breath === true || resp === true){
     document.getElementById("checkUPres").innerHTML = "Call your nearest testing center to schedule a check up.";}
   else if(age >= 65 && pressure === true || heart === true || diabetes === true){
     document.getElementById("checkUPres").innerHTML = "Call your nearest testing center to schedule a check up.";}
