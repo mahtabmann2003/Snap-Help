db06fc7fc36be540a1df626576d377f784916c29
mahtabmann2003 <47609548+mahtabmann2003@users.noreply.github.com>
2020-03-28 13:49:29 -0400

    Gorceyr page added

--- me.js
+++ me.js
@@ -22,46 +22,47 @@ function savelocal () {
 
 }
 
-function validateForm () {
-    name = document.getElementById("fname").value;
-    if (name == "" ) {
-        alert ("Name Must be filled out");
-        return false;
-    }
-    age = document.getElementById("inputage").value;
-    if (inputage == "" )  {
-        alert ("Age Must be filled out");
-        return false;
-    }
-    gender = document.getElementById("gender").value;
-    if (gender == "" ) {
-        alert ("Gender Must be filled out");
-        return false;
-    }
-    country = document.getElementById("country").value;
-    if (country == "" ) {
-        alert ("Country Must be filled out");
-        return false;
-    }
-    city = document.getElementById("inputCity").value;
-    if (city == "" ) {
-        alert ("City Must be filled out");
-        return false;
-    }
-    state = document.getElementById("inputState").value;
-    if (state == "" ) {
-        alert ("State/Province/Region Must be filled out");
-        return false;
-    }
-    zip = document.getElementById("inputZip").value;
-    if (zip == "" ) {
-        alert ("Zip Must be filled out");
-        return false;
-    }
-    street = document.getElementById("autocomplete").value;
-    if (street == "" ) {
-        alert ("Street Must be filled out");
-        return false;
-    }
 
-}
+// function validateForm () {
+//     name = document.getElementById("fname").value;
+//     if (name == "" ) {
+//         alert ("Name Must be filled out");
+//         return false;
+//     }
+//     age = document.getElementById("inputage").value;
+//     if (inputage == "" )  {
+//         alert ("Age Must be filled out");
+//         return false;
+//     }
+//     gender = document.getElementById("gender").value;
+//     if (gender == "" ) {
+//         alert ("Gender Must be filled out");
+//         return false;
+//     }
+//     country = document.getElementById("country").value;
+//     if (country == "" ) {
+//         alert ("Country Must be filled out");
+//         return false;
+//     }
+//     city = document.getElementById("inputCity").value;
+//     if (city == "" ) {
+//         alert ("City Must be filled out");
+//         return false;
+//     }
+//     state = document.getElementById("inputState").value;
+//     if (state == "" ) {
+//         alert ("State/Province/Region Must be filled out");
+//         return false;
+//     }
+//     zip = document.getElementById("inputZip").value;
+//     if (zip == "" ) {
+//         alert ("Zip Must be filled out");
+//         return false;
+//     }
+//     street = document.getElementById("autocomplete").value;
+//     if (street == "" ) {
+//         alert ("Street Must be filled out");
+//         return false;
+//     }
+
+// }
